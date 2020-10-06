2d94a6b4442ee4919fd0ab90888550d558014590
Meissae <meissae@hotmail.fr>
2020-10-06 16:14:28 +0200

    Fin TD3
    TD de BAKARY et FATMI

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +17,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Button rollButton = (Button) findViewById(R.id.button);
 
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(MainActivity.this, "Dé lancé!", Toast.LENGTH_SHORT);
-                toast.show();
+
+                //création du nombre aléatoire1
+                Random rand = new Random();
+                int nombre = rand.nextInt(6 - 1 + 1) + 1;
+                TextView total = (TextView )findViewById(R.id.TextView);
+                total.setText(String.valueOf( nombre ));
+
+                //création du nombre aléatoire2
+
+                int nombre2 = rand.nextInt(6 - 1 + 1) + 1;
+                TextView total2 = (TextView )findViewById(R.id.TextView1);
+                total2.setText(String.valueOf( nombre2 ));
+
             }
         });
+
+
+
     }
+
+
 }
